2bfcd938931cee6f01ebd42834f12cef9892a3b7
caio-vinicius <contato@caiovinicius.dev.br>
2020-12-25 23:30:33 -0300

    pytest: add tests for many endpoints

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -3,12 +3,12 @@ import pytest
 import sys
 sys.path.insert(1, '../')
 
+from endpoints import get_allowed_endpoints, get_paginated_endpoints
 from constants import AUTHORIZATION_CODE
 import app as Client
 
-#def test_home_page(client):
-#    response = client.get('/')
-#    assert response.status_code == 401
+endpoints_allowed = get_allowed_endpoints()
+endpoints_paginated = get_paginated_endpoints()
 
 def test_slash(client):
     """ / endpoint """
@@ -20,6 +20,18 @@ def test_slash_authorized(client):
     """ / endpoint authorized """
 
     r = client.get('/', headers=dict(authorization=AUTHORIZATION_CODE))
+    assert r.status_code == 200
+
+@pytest.mark.parametrize('endpoint', endpoints_allowed)
+def test_allowed_endpoints(client, endpoint):
+    """ all allowed endpoints available on https://api.intra.42.fr/apidoc """
+    r = client.get(f"/42/{endpoint}", headers=dict(authorization=AUTHORIZATION_CODE))
+    assert r.status_code in (200, 0)
+
+@pytest.mark.parametrize('endpoint', endpoints_paginated)
+def test_paginated_endpoints(client, endpoint):
+    """ all paginated endpoints available on https://api.intra.42.fr/apidoc """
+    r = client.get(f"/42/{endpoint}?page=all", headers=dict(authorization=AUTHORIZATION_CODE))
     assert r.status_code == 200
 
 @pytest.fixture
